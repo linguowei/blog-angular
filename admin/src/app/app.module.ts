feecee711ac92239ac1eb7f19deb62437fedd329
linguowei <1368033036@qq.com>
2017-09-05 22:53:15 +0800

    admin

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './pages/add/add.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { ViewComponent } from './pages/view/view.component';
+import { TagComponent } from './pages/tag/tag.component';
+import { ClassificationComponent } from './pages/classification/classification.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ViewComponent,
+    TagComponent,
+    ClassificationComponent
   ],
   imports: [
     BrowserModule,
